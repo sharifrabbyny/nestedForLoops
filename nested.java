63a5e779f32cd668fc4774142cf361cb5cd19a58
sharifrabbyny <139313695+sharifrabbyny@users.noreply.github.com>
2023-12-23 10:34:14 -0500

    Add files via upload

---
+++ nested.java
@@ -0,0 +1,19 @@
+package CopenhagenTransit;
+
+public class nested {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i =1 ; i <= 5; i++) {
+			for (int j = 1; j <= i ; j++) {
+				
+				System.out.print("*");
+			}
+		
+			System.out.println();
+		}
+
+	}
+
+}
